e1a0d7f9e8a0827c8a2cf60295d8b753fa91661d
JonathnSparePartsFuller <jonefuller@gmail.com>
2022-05-10 15:27:56 -0700

    Updated render to have routers and switches

--- src/App.js
+++ src/App.js
@@ -20,8 +20,18 @@ class App extends Component{
   render() {
     return (
       <div>
+      <Router>
         <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/catedit" component={CatEdit} />
+          <Route path="/catindex" component={CatIndex} />
+          <Route path="/catnew" component={CatNew} />
+          <Route path="/catshow" component={CatShow} />
+          <Route component={NotFound} />
+        </Switch>
         <Footer />
+      </Router>
       </div>
     );
   }
