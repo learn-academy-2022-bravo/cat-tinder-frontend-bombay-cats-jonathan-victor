82e9bf4f2cb88ec31bdb2641f2e6ee2bb07ad696
JonathnSparePartsFuller <jonefuller@gmail.com>
2022-05-10 11:40:12 -0700

    Completed Frontend Structure

---
+++ src/Pages/CatNew.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CatNew extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default CatNew;
---
+++ src/Pages/CatShow.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CatShow extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default CatShow;
