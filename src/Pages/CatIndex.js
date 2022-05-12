74054d4821cacca9091c778add2f644cff3dea8c
aVictorWXYZ <victorargote@gmail.com>
2022-05-12 09:41:18 -0700

    fixed typo in CatIndex and Home

--- src/Pages/CatIndex.js
+++ src/Pages/CatIndex.js
@@ -7,7 +7,7 @@ class CatIndex extends Component {
     render() {
         return (
             <div>
-                <h2>Here our a bunch of cats!</h2>
+                <h2>Here are a bunch of cats!</h2>
                 {this.props.cats && this.props.cats.map(cat => {
 
                     return <p key={cat.id}>{cat.name}</p>,
