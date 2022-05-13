8ee2ed5082a8e11ed8540a1283496950e3a9864a
JonathnSparePartsFuller <jonefuller@gmail.com>
2022-05-13 16:31:30 -0700

    Completed MVP, working on stretch goals

--- src/Pages/CatShow.js
+++ src/Pages/CatShow.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-
+import { NavLink } from 'react-router-dom';
+import { Button } from 'reactstrap';
 
 class CatShow extends Component {
     render() {
+        let { cat } = this.props
         return (
             <div>
                 <h1>Check out these fluffy Cats!</h1>
-                <h2>Hi, my name is {this.props.cat.name}!</h2>
-                <img src={this.props.cat.image} alt="adorable cat" height="200px" />
-                <p>I am {this.props.cat.age} years old. I enjoy {this.props.cat.enjoys}.</p>
+                <h2>Hi, my name is {cat && cat.name}!</h2>
+                <img src={cat && cat.image} alt="adorable cat" height="200px" />
+                <p>I am {cat && cat.age} years old. I enjoy {cat && cat.enjoys}.</p>
                 <brake/>
+                <NavLink to="/catindex">
+                    <Button>
+                        Delete Cat Profile
+                    </Button>
+                </NavLink>
             </div>
 
             
