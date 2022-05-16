02d5fab1881aebf69120814de4af8335a85ff208
JonathnSparePartsFuller <jonefuller@gmail.com>
2022-05-16 10:27:46 -0700

    Added style to our webpage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
-
+import { Nav, NavItem } from 'reactstrap'
+import { NavLink } from 'react-router-dom'
 class Header extends Component {
     render() {
+        <Nav className="header-nav">
+
+        <NavItem>
+          <NavLink to="/catindex" className="nav-link">Meet the Cats</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/catnew" className="nav-link">Add a New Cat</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink to="/catshow" className="nav-link">Check Out This Cat</NavLink>
+        </NavItem>
+    </Nav>
+
         return (
             <div>
                 <h2>Fluffy Cat Tinder page </h2>
